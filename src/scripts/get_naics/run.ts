f0bd67dc51292db4ace1bab5c33b2ca69956eb6b
Taylor Rogers <taylor.rea.rogers@gmail.com>
2026-02-07 08:37:30 -0800

    remove get socs and naics

--- src/scripts/get_naics/run.ts
+++
@@ -1,8 +0,0 @@
-import { getDbInstance } from "../../db/index.ts";
-import { getNaics, insertNaicsIntoDb } from "./utils.ts";
-
-const naics = await getNaics();
-
-const db = getDbInstance();
-await insertNaicsIntoDb({ db, naics });
-await db.destroy();
--- src/scripts/get_socs/run.ts
+++
@@ -1,8 +0,0 @@
-import { getDbInstance } from "../../db/index.ts";
-import { getSocs, insertSocsIntoDb } from "./utils.ts";
-
-const socs = await getSocs();
-
-const db = getDbInstance();
-await insertSocsIntoDb({ db, socs });
-await db.destroy();
